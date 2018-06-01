005bc140a40b536d8a76e9eedb44124da60b3fb3
sv_91@inbox.ru <sv_91@inbox.ru>
2018-06-01 11:28:19 +0300

    Еще правки логов

--- src/WebSocketClient.cpp
+++ src/WebSocketClient.cpp
@@ -21,34 +21,34 @@ WebSocketClient::WebSocketClient(QObject *parent)
     const std::string &fileData = readFile(pathToWebSServer);
     m_url = QString::fromStdString(fileData).trimmed();
 
-    CHECK(QObject::connect(&thread1,SIGNAL(started()),this,SLOT(onStarted())), "not connect");
+    CHECK(QObject::connect(&thread1,SIGNAL(started()),this,SLOT(onStarted())), "not connect started");
 
-    CHECK(connect(this, SIGNAL(sendMessage(QString)), this, SLOT(onSendMessage(QString))), "not connect");
+    CHECK(connect(this, SIGNAL(sendMessage(QString)), this, SLOT(onSendMessage(QString))), "not connect sendMessage");
 
-    CHECK(connect(&m_webSocket, &QWebSocket::connected, this, &WebSocketClient::onConnected), "not connect");
-    CHECK(connect(&m_webSocket, &QWebSocket::textMessageReceived, this, &WebSocketClient::onTextMessageReceived), "not connect");
+    CHECK(connect(&m_webSocket, &QWebSocket::connected, this, &WebSocketClient::onConnected), "not connect connected");
+    CHECK(connect(&m_webSocket, &QWebSocket::textMessageReceived, this, &WebSocketClient::onTextMessageReceived), "not connect textMessageReceived");
     CHECK(connect(&m_webSocket, &QWebSocket::disconnected, [this]{
-        LOG << "Wss client disconnected. Thread " << std::this_thread::get_id();
+        LOG << "Wss client disconnected";
         m_webSocket.close();
         if (!isStopped) {
             QTimer::singleShot(milliseconds(10s).count(), this, SLOT(onStarted()));
         }
         isConnected = false;
-    }), "not connect");
+    }), "not connect disconnected");
     CHECK(connect(&thread1, &QThread::finished, [this]{
         LOG << "Wss client finished";
         m_webSocket.close();
-    }), "not connect");
+    }), "not connect finished");
 
     moveToThread(&thread1);
     m_webSocket.moveToThread(&thread1);
 
-    LOG << "Wss client started. Thread " << std::this_thread::get_id() << ". Url " << m_url.toString();
+    LOG << "Wss client started. Url " << m_url.toString();
 }
 
 void WebSocketClient::onStarted() {
     m_webSocket.open(m_url);
-    LOG << "Wss client started2. Thread " << std::this_thread::get_id() << ". Url " << m_url.toString();
+    LOG << "Wss client onStarted. Url " << m_url.toString();
 }
 
 WebSocketClient::~WebSocketClient() {
@@ -65,7 +65,7 @@ void WebSocketClient::start() {
 }
 
 void WebSocketClient::onConnected() {
-    LOG << "Wss client connected. Thread " << std::this_thread::get_id();
+    LOG << "Wss client connected";
     isConnected = true;
 }
 
@@ -73,7 +73,7 @@ void WebSocketClient::onSendMessage(QString message) {
     if (!isConnected) {
         messageQueue.emplace_back(message);
     } else {
-        LOG << "Wss client send message. Thread " << std::this_thread::get_id() << " " << message;
+        LOG << "Wss client send message " << message;
         for (const QString &m: messageQueue) {
             m_webSocket.sendTextMessage(m);
         }
@@ -83,5 +83,5 @@ void WebSocketClient::onSendMessage(QString message) {
 }
 
 void WebSocketClient::onTextMessageReceived(QString message) {
-    LOG << "Wss received. Thread " << std::this_thread::get_id() << " " << message;
+    LOG << "Wss received " << message;
 }
