8446ab1e7780acb4c23d364bfd3679d822480f12
sv_91@inbox.ru <sv_91@inbox.ru>
2018-07-13 09:25:19 +0300

    Поправил run.sh для запуска в папке с русскими буквами

--- deploy/linux/run.sh
+++ deploy/linux/run.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 echo > ./qt.conf
 echo [Paths] >> ./qt.conf
-echo Prefix = $PWD >> ./qt.conf
+echo -n "Prefix = " >> ./qt.conf
+for (( i = 0; i < ${#PWD}; i++ )); do
+c="${PWD:i:1}"
+printf '\\x%04x' "'$c" >> ./qt.conf
+done
+    
+#echo $PWD | 
+#    while read -n 1 u
+#    do [[ -n "$u" ]] && [ "$u" != "/" ] && printf '\\x%04x' "'$u" >> ./qt.conf || [[ -n "$u" ]] && [ "$u" == "/" ] && printf "$u" >> ./qt.conf
+#    done;
+echo >> ./qt.conf
 echo Plugins = plugins >> ./qt.conf
 echo Imports = qml >> ./qt.conf
 echo Qml2Imports = qml >> ./qt.conf
