64ff90264c2950c69a6a25308db5c0bc340a8206
sv_91@inbox.ru <sv_91@inbox.ru>
2018-08-22 17:38:06 +0300

    Поправил работу с потоками в клиенте

--- src/client.cpp
+++ src/client.cpp
@@ -32,10 +32,13 @@ void SimpleClient::moveToThread(QThread *thread) {
 
 void SimpleClient::startTimer() {
     if (timer == nullptr) {
-        timer = new QTimer(this);
+        timer = new QTimer();
         CHECK(connect(timer, SIGNAL(timeout()), this, SLOT(onTimerEvent())), "not connect timeout");
         if (thread1 != nullptr) {
+            timer->setParent(thread1);
             CHECK(timer->connect(thread1, SIGNAL(finished()), SLOT(stop())), "not connect finished");
+        } else {
+            timer->setParent(this);
         }
         timer->setInterval(milliseconds(1s).count());
         timer->start();
