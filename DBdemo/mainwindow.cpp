48077e7edb4730654338caabfc1889b503a5c206
Igor Janssen <ijanssen@ics.com>
2018-09-05 16:45:44 +0700

    QSettings serialization demo

--- DBdemo/mainwindow.cpp
+++ DBdemo/mainwindow.cpp
@@ -17,11 +17,40 @@
 #include <vector>
 #include "HttpClient.h"
 
+#include <QSettings>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    {
+        QList<TestStructure> list;
+        TestStructure t;
+        t.name = "John Smith";
+        t.type = 42;
+        t.value = 3443.34;
+        list.append(t);
+        t.name = "jfdjkkjfkjfg";
+        t.type = 22;
+        t.value = -133.224;
+        list.append(t);
+        QSettings s("test.ini", QSettings::IniFormat);
+        s.setValue("test", QVariant::fromValue(list));
+    }
+
+    {
+        QList<TestStructure> list;
+        QSettings s("test.ini", QSettings::IniFormat);
+        QVariant value = s.value("test");
+        list = value.value<QList<TestStructure>>();
+        foreach (const TestStructure &t, list)
+            qDebug() << t.name << t.type << t.value;
+    }
+
+    return;
 
     qDebug() << sizeof(transactions::Transaction);
 
@@ -31,24 +60,24 @@ MainWindow::MainWindow(QWidget *parent) :
     quint64 res = 0;
 
     for (int k = 0; k < 20; k++) {
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    //std::list<transactions::Transaction> container;
-    std::vector<transactions::Transaction> container;
-    container.reserve(11000);
-    for (int n = 0; n < NUM; n++) {
-        transactions::Transaction t;
-        t.address = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
-        t.value = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
-        t.from = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
-        t.to = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
-        t.fee = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
-        container.push_back(t);
-    }
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        //std::list<transactions::Transaction> container;
+        std::vector<transactions::Transaction> container;
+        container.reserve(11000);
+        for (int n = 0; n < NUM; n++) {
+            transactions::Transaction t;
+            t.address = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
+            t.value = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
+            t.from = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
+            t.to = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
+            t.fee = QStringLiteral("ldkldfkldfklgfklgfklgfklgf");
+            container.push_back(t);
+        }
 
-    std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+        std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
 
-    //std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
-    //std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() <<std::endl;
+        //std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
+        //std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() <<std::endl;
         res += std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
     }
     qDebug() << res / 20;
@@ -56,17 +85,17 @@ MainWindow::MainWindow(QWidget *parent) :
     HttpSimpleClient *client = new HttpSimpleClient();
     client->sendMessagePost(QUrl("http://www.google.com:80/jkjktrjkt"), "{\"jsonrpc\":\"2.0\",\"method\":\"mhc_send\",\"params\":{\"data\":\"\",\"fee\":\"\",\"nonce\":\"3\",\"pubkey\":\"3059301306072A8648CE3D020106082A8648CE3D030107034200047FAF41D8AC6C965467EC24A8B78E528892DFEBAA91EEE770443EA574F4A384D0C305ED6FE97A2652241C4F6E2EE7A2306354EF39AFAAE2C0156C7FF69ECE3110\",\"sign\":\"3044022035538e5d047d394b58cb3046d4bc387d456cf850bcfe5231244438e98740acf8022045dc2d1750689f820c43ef26a4058cdbcaa5e5dbd2e7e80115baf3639989587b\",\"to\":\"0x00caceded040cdbfcc6bc4f43a72133252f4cb402478356dff\",\"value\":\"1\"}}",
                             [](const std::string &response, const TypedException &exception) {
-                LOG << "Ups " << response;
-            });
+        LOG << "Ups " << response;
+    });
     const QString message = "{\"jsonrpc\":\"2.0\",\"method\":\"mhc_send\",\"params\":{\"data\":\"\",\"fee\":\"\",\"nonce\":\"3\",\"pubkey\":\"3059301306072A8648CE3D020106082A8648CE3D030107034200047FAF41D8AC6C965467EC24A8B78E528892DFEBAA91EEE770443EA574F4A384D0C305ED6FE97A2652241C4F6E2EE7A2306354EF39AFAAE2C0156C7FF69ECE3110\",\"sign\":\"636c61737320436f6e7472616374200a7b20200a09636f6e7374727563746f722829200a097b200a0909746869732e6f776e6572203d206d73672e73656e6465723b0a0909746869732e6d696e56616c7565203d20303b0a0909746869732e616d6f756e74203d206d73672e76616c75653b0a0909746869732e7061796d656e744c697374203d206e6577204d61703b0a0909746869732e6e616d654c697374203d206e6577204d61703b0a097d200a097061792829200a097b200a0909696620286d73672e76616c7565203c20746869732e6d696e56616c7565290a09097b200a0909097468726f7720747275653b0a09097d0a0909656c73650a09097b200a0909096c657420637572416472657373416d6f756e74203d20746869732e7061796d656e744c6973742e676574286d73672e73656e646572293b0a0909096c6574206375724d736756616c7565203d206d73672e76616c75653b0a090909696628637572416472657373416d6f756e7420213d3d20756e646566696e656420262620747970656f6620637572416472657373416d6f756e74203d3d3d20276e756d6265709097b200a0909097468726f7720747275653b0a09097d0a0909656c73650a09097b200a0909096c657420637572416472657373416d6f756e74203d20746869732e7061796d656e744c6973742e676574286d73672e73656e646572293b0a0909096c6574206375724d736756616c7565203d206d73672e76616c75653b0a074203d3d3d20276e756d6265709097b200a0909097468726f7720747275653b0a09097d0a0909656c73650a09097b200a0909096c657420637572416472657373416d6f756e74203d20746869732e7061796d656e744c693d3d3d20276e76d6f756e74203d20746869732e7061796d656e744c6973742e676574286d73672e73656e646572293b0a0909096c6574206374206375724d736756616c7565203d206d73672e76616c75653b0a074203d3d3d20276e756d6265709097b200a0909097468726f7720747275653b0a09097d0a0909656c73650a09097b200a0909096c657420637572416472657373416d6f756e74203d20746869732e7061796d656e744c6973742e676574286d73672e73656e646572293b0a0909096c6574206375724d736756616c7565203d206d73672e76616c75653b0a090909696628637572416472657373416d6f756e7420213d3d20756e646566696e656420262620747970656f6620637572416472657373416d6f756e74203d3d3d20276e756d6265709097b200a0909097468726f7720747275653b0a09097d0a0909656c73650a09097b200a0909096c657420637572416472657373416d6f756e74203d20746869732e7061796d656e744c6973742e676574286d73672e73656e646572293b0a0909096c6574206375724d736756616c7565203d206d73672e76616c75653b0a090909696628637572416472657373416d6f756e7420213d3d20756e646566696e656420262620747970656f6620637572416472657373416d6f756e74203d3d3d20276e756d62657\",\"to\":\"0x00caceded040cdbfcc6bc4f43a72133252f4cb402478356dff\",\"value\":\"1\"}}";
     client->sendMessagePost(QUrl("http://188.246.233.140:9999/"), message,
                             [](const std::string &response, const TypedException &exception) {
-                LOG << "Ups " << response;
-            });
+        LOG << "Ups " << response;
+    });
 
     HttpSocket *httpsocket = new HttpSocket(QUrl("http://188.246.233.140:9999/"), message);
-BEGIN_SLOT_WRAPPER
-    MessengerDBStorage db;
+    BEGIN_SLOT_WRAPPER
+            MessengerDBStorage db;
     //db.openDB();
     db.init();
 
@@ -75,16 +104,16 @@ BEGIN_SLOT_WRAPPER
     qDebug() << db.getSettings("key1");
     qDebug() << db.getSettings("key2");
 
-//    db.addPayment("818af8b3a50a5b6758360d11a44533f596af76feb9b4e57557220a8536c38165",
-//                                      "818af8b3a50a5b6758360d11a44533f596af76feb9b4e57557220a8536c38165",
-//                                      "1CKZ3fJrojYauQHUZiuSKeuvw1AwBVUwHX",
-//                                      "19BWPGPJM9KAG6r8vRsLH9e6w495dx3C5W,1",
-//                                      "0.0006650",
-//                                      "66502",
-//                                      533227,
-//                                      true,
-//                                      1532329669,
-//                                      "7");
+    //    db.addPayment("818af8b3a50a5b6758360d11a44533f596af76feb9b4e57557220a8536c38165",
+    //                                      "818af8b3a50a5b6758360d11a44533f596af76feb9b4e57557220a8536c38165",
+    //                                      "1CKZ3fJrojYauQHUZiuSKeuvw1AwBVUwHX",
+    //                                      "19BWPGPJM9KAG6r8vRsLH9e6w495dx3C5W,1",
+    //                                      "0.0006650",
+    //                                      "66502",
+    //                                      533227,
+    //                                      true,
+    //                                      1532329669,
+    //                                      "7");
     db.addMessage("1234", "3454", "abcd", 1, 4000, true, true, true, "asdfdf", 1);
     db.addMessage("1234", "3454", "abcd", 1, 4000, true, true, true, "asdfdf", 1);
     db.addMessage("1234", "3454", "abcd", 1, 4000, true, true, true, "asdfdf", 1);
@@ -165,7 +194,7 @@ BEGIN_SLOT_WRAPPER
     tdb.addPayment("mh", "gfklklkltjjkguieriufhg", "address100", true, "user7", "user1", "100", 568869445334, "nvcmnjkdfjkgf", "100", 8896865);
     tdb.addPayment("mh", "gfklklklruuiuiduidgjkg", "address100", false, "user7", "user3", "2340", 568869455856, "nvcmnjkdfjkgf", "100", 8896865);
     qDebug() << tdb.getPaymentsForAddress("address", "mh", 0, 3, true).size();
-/*
+    /*
 
     //9223372036854775807
     //100000000000000000000
@@ -206,17 +235,40 @@ BEGIN_SLOT_WRAPPER
     r = r - num1;
     r = r - num1;
     qDebug() << r.getDecimal();
-END_SLOT_WRAPPER
+    END_SLOT_WRAPPER
 
-//    QTcpSocket
-//        SimpleClient client;
-//        client.sendMessagePost(QUrl("http://188.246.233.140:9999/"), "{\"jsonrpc\":\"2.0\",\"method\":\"mhc_send\",\"params\":{\"data\":\"\",\"fee\":\"\",\"nonce\":\"3\",\"pubkey\":\"3059301306072A8648CE3D020106082A8648CE3D030107034200047FAF41D8AC6C965467EC24A8B78E528892DFEBAA91EEE770443EA574F4A384D0C305ED6FE97A2652241C4F6E2EE7A2306354EF39AFAAE2C0156C7FF69ECE3110\",\"sign\":\"3044022035538e5d047d394b58cb3046d4bc387d456cf850bcfe5231244438e98740acf8022045dc2d1750689f820c43ef26a4058cdbcaa5e5dbd2e7e80115baf3639989587b\",\"to\":\"0x00caceded040cdbfcc6bc4f43a72133252f4cb402478356dff\",\"value\":\"1\"}}", [](const std::string &response) {
-//            LOG << "Ups";
-//        });
+            //    QTcpSocket
+            //        SimpleClient client;
+            //        client.sendMessagePost(QUrl("http://188.246.233.140:9999/"), "{\"jsonrpc\":\"2.0\",\"method\":\"mhc_send\",\"params\":{\"data\":\"\",\"fee\":\"\",\"nonce\":\"3\",\"pubkey\":\"3059301306072A8648CE3D020106082A8648CE3D030107034200047FAF41D8AC6C965467EC24A8B78E528892DFEBAA91EEE770443EA574F4A384D0C305ED6FE97A2652241C4F6E2EE7A2306354EF39AFAAE2C0156C7FF69ECE3110\",\"sign\":\"3044022035538e5d047d394b58cb3046d4bc387d456cf850bcfe5231244438e98740acf8022045dc2d1750689f820c43ef26a4058cdbcaa5e5dbd2e7e80115baf3639989587b\",\"to\":\"0x00caceded040cdbfcc6bc4f43a72133252f4cb402478356dff\",\"value\":\"1\"}}", [](const std::string &response) {
+            //            LOG << "Ups";
+            //        });
 
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+QDataStream &operator<<(QDataStream &out, const TestStructure &obj)
+{
+    out << obj.currency
+        << obj.address
+        << obj.type
+        << obj.group
+        << obj.name
+        << obj.value;
+
+    return out;
+}
+
+QDataStream &operator>>(QDataStream &in, TestStructure &obj)
+{
+    in >> obj.currency
+        >> obj.address
+        >> obj.type
+        >> obj.group
+        >> obj.name
+        >> obj.value;
+    return in;
 }
