7b101edde7cb8e4eb29b4ed76ffa08f5da309c09
sv_91@inbox.ru <sv_91@inbox.ru>
2018-09-10 12:30:30 +0300

    Подправил скрипт deploy-я

--- deploy/linux/deploy/deploy.sh
+++ deploy/linux/deploy/deploy.sh
@@ -2,4 +2,4 @@
 
 export PATH=/opt/Qt/5.10.1/gcc_64/bin/:$PATH
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/Qt/5.10.1/gcc_64/lib/"
-./linuxdeployqt-continuous-x86_64.AppImage $1 -extra-plugins=imageformats/libqsvg.so,iconengines/libqsvgicon.so
+./linuxdeployqt-continuous-x86_64.AppImage $1 -extra-plugins=imageformats/libqsvg.so,iconengines/libqsvgicon.so -exclude-libs=libqsqlibase,libqsqlodbc,libqsqlpsql,libqsqltds
