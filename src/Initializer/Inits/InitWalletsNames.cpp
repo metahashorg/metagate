5843adb956d7d7e2d4277da4cd69a8098024a11a
sv_91@inbox.ru <sv_91@inbox.ru>
2019-04-15 14:57:24 +0300

    Продолжаю добавлять взаимодействие с сервером

--- src/Initializer/Inits/InitWalletsNames.cpp
+++ src/Initializer/Inits/InitWalletsNames.cpp
@@ -50,12 +50,12 @@ void InitWalletsNames::sendInitSuccess(const TypedException &exception) {
     sendState("init", false, exception);
 }
 
-InitWalletsNames::Return InitWalletsNames::initialize(std::shared_future<MainWindow*> mainWindow, std::shared_future<JavascriptWrapper*> jsWrap) {
+InitWalletsNames::Return InitWalletsNames::initialize(std::shared_future<MainWindow*> mainWindow, std::shared_future<JavascriptWrapper*> jsWrap, std::shared_future<std::pair<auth::Auth*, auth::AuthJavascript*>> auth) {
     const TypedException exception = apiVrapper2([&, this] {
         database = std::make_unique<wallet_names::WalletNamesDbStorage>(getDbPath());
         database->init();
 
-        manager = std::make_unique<wallet_names::WalletNames>(*database, *(jsWrap.get()));
+        manager = std::make_unique<wallet_names::WalletNames>(*database, *(jsWrap.get()), *auth.get().first);
         manager->start();
 
         javascript = std::make_unique<wallet_names::WalletNamesJavascript>(*manager);
--- src/WalletNames/WalletNames.cpp
+++ src/WalletNames/WalletNames.cpp
@@ -2,13 +2,22 @@
 
 #include <set>
 
+#include <QSettings>
+#include <QJsonDocument>
+
 #include "Log.h"
 #include "QRegister.h"
+#include "Paths.h"
 #include "SlotWrapper.h"
 
 #include "JavascriptWrapper.h"
 
+#include "auth/Auth.h"
+
+#include "machine_uid.h"
+
 #include "WalletNamesDbStorage.h"
+#include "WalletNamesMessages.h"
 
 using namespace std::placeholders;
 
@@ -21,12 +30,23 @@ const static QString WALLET_TYPE_MTH = "mth";
 const static QString WALLET_TYPE_BTC = "btc";
 const static QString WALLET_TYPE_ETH = "eth";
 
-WalletNames::WalletNames(WalletNamesDbStorage &db, JavascriptWrapper &javascriptWrapper)
+static QString getWssServer() {
+    QSettings settings(getSettingsPath(), QSettings::IniFormat);
+    CHECK(settings.contains("wallet_names/server"), "settings wallet_names/server not found");
+    return settings.value("wallet_names/server").toString();
+}
+
+WalletNames::WalletNames(WalletNamesDbStorage &db, JavascriptWrapper &javascriptWrapper, auth::Auth &authManager)
     : TimerClass(5min, nullptr)
     , db(db)
     , javascriptWrapper(javascriptWrapper)
+    , authManager(authManager)
+    , client(getWssServer())
 {
     CHECK(connect(this, &WalletNames::callbackCall, this, &WalletNames::onCallbackCall), "not connect onCallbackCall");
+
+    CHECK(connect(&client, &WebSocketClient::messageReceived, this, &WalletNames::onWssMessageReceived), "not connect wssClient");
+    CHECK(connect(&authManager, &auth::Auth::logined, this, &WalletNames::onLogined), "not connect onLogined");
 
     CHECK(connect(this, &WalletNames::timerEvent, this, &WalletNames::onTimerEvent), "not connect onTimerEvent");
     CHECK(connect(this, &WalletNames::startedEvent, this, &WalletNames::onRun), "not connect run");
@@ -44,7 +64,11 @@ WalletNames::WalletNames(WalletNamesDbStorage &db, JavascriptWrapper &javascript
 
     signalFunc = std::bind(&WalletNames::callbackCall, this, _1);
 
+    emit authManager.reEmit();
+
     moveToThread(&thread1); // TODO вызывать в TimerClass
+
+    client.start();
 }
 
 void WalletNames::onCallbackCall(WalletNames::Callback callback) {
@@ -55,25 +79,28 @@ END_SLOT_WRAPPER
 
 void WalletNames::onRun() {
 BEGIN_SLOT_WRAPPER
+    getAllWallets();
 END_SLOT_WRAPPER
 }
 
 void WalletNames::onTimerEvent() {
 BEGIN_SLOT_WRAPPER
+    getAllWallets();
 END_SLOT_WRAPPER
+}
+
+void WalletNames::getAllWallets() {
+    const QString message = makeGetWalletsMessage(id.get(), token, hwid);
+    stateRequest = StateRequest::Requested;
+    emit client.sendMessage(message);
 }
 
 void WalletNames::onAddOrUpdateWallets(const std::vector<WalletInfo> &infos, const AddWalletsNamesCallback &callback) {
 BEGIN_SLOT_WRAPPER
     const TypedException &exception = apiVrapper2([&]{
         for (const WalletInfo &info: infos) {
-            const bool isUpdated = db.addOrUpdateWallet(info);
-            if (isUpdated) {
-                emit updatedWalletName(info.address, info.name);
-            }
+            db.addOrUpdateWallet(info);
         }
-
-        emit walletsFlushed();
     });
     callback.emitFunc(exception);
 END_SLOT_WRAPPER
@@ -83,6 +110,11 @@ void WalletNames::onSaveWalletName(const QString &address, const QString &name, 
 BEGIN_SLOT_WRAPPER
     const TypedException &exception = apiVrapper2([&]{
         db.giveNameWallet(address, name);
+        const QString message = makeRenameMessage(address, name, id.get(), token, hwid);
+        if (stateRequest == StateRequest::Requested) {
+            stateRequest = StateRequest::Intercepted;
+        }
+        emit client.sendMessage(message);
     });
     callback.emitFunc(exception);
 END_SLOT_WRAPPER
@@ -192,6 +224,92 @@ BEGIN_SLOT_WRAPPER
     });
     if (exception.isSet()) {
         callback.emitException(exception);
+    }
+END_SLOT_WRAPPER
+}
+
+void WalletNames::processWalletsList(const std::vector<WalletInfo> &wallets) {
+    if (stateRequest == StateRequest::Intercepted) {
+        return;
+    }
+
+    stateRequest = StateRequest::NotRequest;
+
+    for (const WalletInfo &info: wallets) {
+        const bool isUpdated = db.addOrUpdateWallet(info);
+        if (isUpdated) {
+            emit updatedWalletName(info.address, info.name);
+        }
+    }
+
+    emit walletsFlushed();
+
+    sendAllWallets();
+}
+
+void WalletNames::sendAllWallets() {
+    const auto processWallets = [this](const JavascriptWrapper::WalletType &type, const QString &hwid, const QString &userName, const std::vector<QString> &walletAddresses) {
+        const QString typeStr = walletTypeToStr(type);
+
+        std::vector<WalletInfo> thisWallets;
+        thisWallets.reserve(walletAddresses.size());
+        for (const QString &address: walletAddresses) {
+            WalletInfo info = db.getWalletInfo(address);
+            info.address = address; // На случай, если вернулся пустой результат
+            info.infos.emplace_back(userName, hwid, typeStr);
+            thisWallets.emplace_back(info);
+        }
+
+        return thisWallets;
+    };
+
+    const std::vector<JavascriptWrapper::WalletType> types = {JavascriptWrapper::WalletType::Tmh, JavascriptWrapper::WalletType::Mth, JavascriptWrapper::WalletType::Btc, JavascriptWrapper::WalletType::Eth};
+    for (const JavascriptWrapper::WalletType type: types) {
+        emit javascriptWrapper.getListWallets(type, JavascriptWrapper::WalletsListCallback([this, type, processWallets](const QString &hw, const QString &userName, const std::vector<QString> &walletAddresses) mutable {
+            const std::vector<WalletInfo> wallets = processWallets(type, hw, userName, walletAddresses);
+            const QString message = makeSetWalletsMessage(wallets, id.get(), token, hwid);
+            client.sendMessage(message);
+        }, [](const TypedException &e) {
+            LOG << "Error: " << e.description;
+        }, signalFunc));
+    }
+}
+
+void WalletNames::onWssMessageReceived(QString message) {
+BEGIN_SLOT_WRAPPER
+    const QJsonDocument messageJson = QJsonDocument::fromJson(message.toUtf8());
+    const ResponseType responseType = getMethodAndAddressResponse(messageJson);
+
+    if (responseType.isError) {
+        LOG << "WalletNames response error " << responseType.id << " " << responseType.method << " " << responseType.error;
+        return;
+    }
+
+    if (responseType.method == METHOD::RENAME) {
+        LOG << "Rename wallet ok";
+    } else if (responseType.method == METHOD::SET_WALLETS) {
+        LOG << "Set wallets ok";
+    } else if (responseType.method == METHOD::GET_WALLETS) {
+        LOG << "Get wallets ok";
+        const std::vector<WalletInfo> wallets = parseGetWalletsMessage(messageJson);
+        processWalletsList(wallets);
+    } else {
+        throwErr("Incorrect response type");
+    }
+END_SLOT_WRAPPER
+}
+
+void WalletNames::onLogined(bool isInit, const QString login) {
+BEGIN_SLOT_WRAPPER
+    if (isInit) {
+        authManager.getLoginInfo(auth::Auth::LoginInfoCallback([this](const auth::LoginInfo &info) {
+            token = info.token;
+            hwid = QString::fromStdString(getMachineUid());
+
+            getAllWallets();
+        }, [](const TypedException &e) {
+            LOG << "Error: " << e.description;
+        }, signalFunc));
     }
 END_SLOT_WRAPPER
 }
