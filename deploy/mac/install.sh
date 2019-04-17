e71865f2fdac1c5ff8fa6bbce55a6ca40260126e
Igor Janssen <alaves17@gmail.com>
2019-04-17 11:36:50 +0700

    Fix macos installation

--- deploy/mac/install.sh
+++ deploy/mac/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-sed "s?sssssss?${PWD}/MetaGate?g" < ./mgtemp.plist > ~/Library/LaunchAgents/com.metahash.metagate.plist
+DIR=$(dirname "$0")
+sed "s?sssssss?$DIR/MetaGate?g" < $DIR/mgtemp.plist > ~/Library/LaunchAgents/com.metahash.metagate.plist
