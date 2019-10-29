9ad439b27bd73697f7d0820e870070bca9451479
Igor Janssen <alaves17@gmail.com>
2019-10-29 13:34:37 +0700

    Refactoring

--- src/MetaGate/MetaGateMessages.cpp
+++ src/MetaGate/MetaGateMessages.cpp
@@ -16,15 +16,15 @@ namespace metagate {
 
 QString makeCommandLineMessageForWss(const QString &hardwareId, const QString &userId, size_t focusCount, const QString &line, bool isEnter, bool isUserText) {
     QJsonObject allJson;
-    allJson.insert("app", "MetaSearch");
+    allJson.insert(QStringLiteral("app"), QStringLiteral("MetaSearch"));
     QJsonObject data;
-    data.insert("machine_uid", hardwareId);
-    data.insert("user_id", userId);
-    data.insert("focus_release_count", static_cast<int>(focusCount));
-    data.insert("text", QString(line.toUtf8().toHex()));
-    data.insert("is_enter_pressed", isEnter);
-    data.insert("is_user_text", isUserText);
-    allJson.insert("data", data);
+    data.insert(QStringLiteral("machine_uid"), hardwareId);
+    data.insert(QStringLiteral("user_id"), userId);
+    data.insert(QStringLiteral("focus_release_count"), static_cast<int>(focusCount));
+    data.insert(QStringLiteral("text"), QString(line.toUtf8().toHex()));
+    data.insert(QStringLiteral("is_enter_pressed"), isEnter);
+    data.insert(QStringLiteral("is_user_text"), isUserText);
+    allJson.insert(QStringLiteral("data"), data);
     QJsonDocument json(allJson);
 
     return json.toJson(QJsonDocument::Compact);
@@ -32,37 +32,37 @@ QString makeCommandLineMessageForWss(const QString &hardwareId, const QString &u
 
 QString makeMessageApplicationForWss(const QString &hardwareId, const QString &utmData, const QString &userId, const QString &applicationVersion, const QString &interfaceVersion, bool isForgingActive, const std::vector<QString> &keysTmh, const std::vector<QString> &keysMth, bool isVirtualMachine, const QString &osName) {
     QJsonObject allJson;
-    allJson.insert("app", "MetaGate");
+    allJson.insert(QStringLiteral("app"), QStringLiteral("MetaGate"));
     QJsonObject data;
-    data.insert("machine_uid", hardwareId);
-    data.insert("utm_data", utmData);
-    data.insert("user_id", userId);
-    data.insert("application_ver", applicationVersion);
-    data.insert("interface_ver", interfaceVersion);
-    data.insert("is_virtual", isVirtualMachine);
-    data.insert("isForgingActive", isForgingActive);
-    data.insert("os_name", osName);
+    data.insert(QStringLiteral("machine_uid"), hardwareId);
+    data.insert(QStringLiteral("utm_data"), utmData);
+    data.insert(QStringLiteral("user_id"), userId);
+    data.insert(QStringLiteral("application_ver"), applicationVersion);
+    data.insert(QStringLiteral("interface_ver"), interfaceVersion);
+    data.insert(QStringLiteral("is_virtual"), isVirtualMachine);
+    data.insert(QStringLiteral("isForgingActive"), isForgingActive);
+    data.insert(QStringLiteral("os_name"), osName);
 
     QJsonArray keysTmhJson;
     for (const QString &key: keysTmh) {
         keysTmhJson.push_back(key);
     }
-    data.insert("keys_tmh", keysTmhJson);
+    data.insert(QStringLiteral("keys_tmh"), keysTmhJson);
 
     QJsonArray keysMthJson;
     for (const QString &key: keysMth) {
         keysMthJson.push_back(key);
     }
-    data.insert("keys_mth", keysMthJson);
+    data.insert(QStringLiteral("keys_mth"), keysMthJson);
 
-    allJson.insert("data", data);
+    allJson.insert(QStringLiteral("data"), data);
     QJsonDocument json(allJson);
 
     return json.toJson(QJsonDocument::Compact);
 }
 
 QString metaOnlineMessage() {
-    return "{\"app\":\"MetaOnline\"}";
+    return QStringLiteral("{\"app\":\"MetaOnline\"}");
 }
 
 
@@ -104,14 +104,14 @@ QString parseAppType(const QJsonDocument &response)
 
 QString parseMetaOnlineResponse(const QJsonDocument &response) {
     const QJsonObject root = response.object();
-    if (!root.contains("app") || !root.value("app").isString()) {
+    if (!root.contains(QLatin1String("app")) || !root.value(QLatin1String("app")).isString()) {
         return "";
     }
-    const QString appType = root.value("app").toString();
+    const QString appType = root.value(QLatin1String("app")).toString();
 
-    if (appType == QStringLiteral("MetaOnline")) {
-        if (root.contains("data") && root.value("data").isObject()) {
-            const QJsonObject data = root.value("data").toObject();
+    if (appType == QLatin1String("MetaOnline")) {
+        if (root.contains(QLatin1String("data")) && root.value(QLatin1String("data")).isObject()) {
+            const QJsonObject data = root.value(QLatin1String("data")).toObject();
             return QJsonDocument(data).toJson(QJsonDocument::JsonFormat::Compact);
         };
     }
@@ -120,29 +120,29 @@ QString parseMetaOnlineResponse(const QJsonDocument &response) {
 
 std::pair<QString, QString> parseShowExchangePopupResponse(const QJsonDocument &response) {
     const QJsonObject root = response.object();
-    if (!root.contains("app") || !root.value("app").isString()) {
-        return std::make_pair("", "");
+    if (!root.contains(QLatin1String("app")) || !root.value(QLatin1String("app")).isString()) {
+        return std::make_pair(QString(), QString());
     }
-    const QString appType = root.value("app").toString();
+    const QString appType = root.value(QLatin1String("app")).toString();
     if (appType == QStringLiteral("InEvent")) {
-        const QString event = root.value("event").toString();
-        if (event == QStringLiteral("showExchangePopUp")) {
-            const QString user = root.value("user").toString();
-            const QString type = root.value("type").toString();
+        const QString event = root.value(QLatin1String("event")).toString();
+        if (event == QLatin1String("showExchangePopUp")) {
+            const QString user = root.value(QLatin1String("user")).toString();
+            const QString type = root.value(QLatin1String("type")).toString();
             return std::make_pair(user, type);
         }
     }
-    return std::make_pair("", "");
+    return std::make_pair(QString(), QString());
 }
 
 QString parseTestTorrentRequest(const QJsonDocument &response, QUrl &url, std::vector<std::pair<QString, QString>> &addresses)
 {
     QString id;
     const QJsonObject root = response.object();
-    if (!root.contains("app") || !root.value("app").isString()) {
+    if (!root.contains(QLatin1String("app")) || !root.value(QLatin1String("app")).isString()) {
         return id;
     }
-    const QString appType = root.value("app").toString();
+    const QString appType = root.value(QLatin1String("app")).toString();
     if (appType == QLatin1String("TestTorrent")) {
         CHECK(root.contains(QLatin1String("id")) && root.value(QLatin1String("id")).isString(), "id field not found");
         id = root.value(QLatin1String("id")).toString();
