406412395013efe88a7a6d6d5c0f8bdbe3ca1cc4
Igor Janssen <alaves17@gmail.com>
2019-12-05 15:46:18 +0700

    Remove unused

--- deploy/mac/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DIR=$(dirname "$0")
-sed "s?sssssss?$DIR/MetaGate.app/Contents/MacOS/MetaGate?g" < $DIR/mgtemp.plist > ~/Library/LaunchAgents/com.metahash.metagate.plist
--- deploy/mac/install_old.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DIR=$(dirname "$0")
-sed "s?sssssss?$DIR/MetaGate?g" < $DIR/mgtemp.plist > ~/Library/LaunchAgents/com.metahash.metagate.plist
